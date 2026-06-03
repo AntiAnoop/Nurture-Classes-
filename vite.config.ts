01177a3331bd0bdce33f12defe81ad828974a263
AntiAnoop <77173813+AntiAnoop@users.noreply.github.com>
2026-06-03 13:17:46 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,18 @@ import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import {defineConfig} from 'vite';
+import { createClient } from '@supabase/supabase-js';
+
+// Vite requires 'import.meta.env' and a 'VITE_' prefix to expose variables to the browser
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY
+
+if (!supabaseUrl || !supabaseKey) {
+  console.error("Supabase environment variables are missing!")
+}
+
+export const supabase = createClient(supabaseUrl, supabaseKey)
+
 
 export default defineConfig(() => {
   return {
